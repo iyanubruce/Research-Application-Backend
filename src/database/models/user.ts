79caa20c15b5367c89b000454b0de2f74ea90e08
Iyanu <iyanuoluwa938@gmail.com>
2024-11-12 11:21:56 +0100

    feat: created user model

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,1 +1,41 @@
 import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  courses: [
+    {
+      courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+      },
+      lastAccessed: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  lastLogin: {
+    type: Date,
+    default: Date.now
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
